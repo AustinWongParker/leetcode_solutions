3d3360945ffc0f3878d79c6c7ecf9447e23b1cab
AustinWongParker <wongparker97@gmail.com>
2024-02-24 21:54:29 -0600

    second attempt

---
+++ ContainsDuplicate/ContainsDuplicate_second_attempt.py
@@ -0,0 +1,28 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        # second attempt
+        # solved in ~15-20mins
+
+        # store our values in a hashmap - { 1 : 2 }
+        # For example 1
+        #                                  ^ the value 1, shows up twice
+        my_dictionary = {}
+
+        # iterate
+        for i in nums:
+            # if i is in the dictionary, return False immediately
+            if my_dictionary.get(i):
+                return True
+            else:
+                # add the value into the dictionary
+                my_dictionary[i] = nums[i-1]
+        
+        # if this return is reached, we know the int array nums has no duplicate values
+        return False 
+
+        # if every element is distinct (every value is unique)
+        #return False
+
+        # if any value appears at least twice
+        #return True
