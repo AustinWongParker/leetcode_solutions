f9c584885bf8c22110b7371ee3e8f4a3fe3a1e43
AustinWongParker <wongparker97@gmail.com>
2024-04-03 20:25:47 -0500

    generate and polish

---
+++ ReversePolishNotation/solution_neetcode.py
@@ -0,0 +1,19 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b-a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b/a))
+            else:
+                stack.append(int(c)) #convert it to a number when we return it
+
+        return stack[0]
+            
